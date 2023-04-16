af22b79ed5cc23626beac52c174e2bf32bc64321
Nehir Gencay <nhrgncy@gmail.com>
2023-04-17 00:20:37 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = { 
+        name: "Sherlock Holmes",
+        streetAdress : "221B Baker Street"
+    };
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const myObject = {...employee};
+    myObject[key] = value;
+    return myObject;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
